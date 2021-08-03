20fb1577237245ede53cb3827495573449fa2784
codingbrains8055 <aniketsingh12899@gmail.com>
2021-08-03 23:15:13 +0530

    add classes in some files

--- JavaFundamentals(InClass)/startCoding.java
+++ JavaFundamentals(InClass)/startCoding.java
@@ -1,5 +1,13 @@
-static void  printHelloWorld()
-{
-   // your code here
-   System.out.println("Hello World");
+/**
+ * startCoding
+ */
+public class startCoding {
+   public static void main(String[] args) {
+      printHelloWorld();
+   }
+   
+   static void printHelloWorld() {
+      // your code here
+      System.out.println("Hello World");
+   }
 }
